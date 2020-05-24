4de1c1013f8f057391cd8006687f0796f30fc28b
JimmyLin <lexusjimmy2002@gmail.com>
2020-05-24 19:26:20 +0900

    Updates

---
+++ precache-manifest.43d7cc3e1c48fe1f5f5057cac77ea899.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a22f8660a1c2903c8ddbc0140f56112a",
+    "url": "/react-score-pwa/index.html"
+  },
+  {
+    "revision": "26b6182a664f49d233ba",
+    "url": "/react-score-pwa/static/css/main.c3c966a2.chunk.css"
+  },
+  {
+    "revision": "881696cb03fca59f0f72",
+    "url": "/react-score-pwa/static/js/2.6641af4c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/react-score-pwa/static/js/2.6641af4c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26b6182a664f49d233ba",
+    "url": "/react-score-pwa/static/js/main.9b8ce679.chunk.js"
+  },
+  {
+    "revision": "4cbbaea7a28f66770016",
+    "url": "/react-score-pwa/static/js/runtime-main.15bf8675.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-score-pwa/static/media/logo.5d5d9eef.svg"
+  }
+]);
